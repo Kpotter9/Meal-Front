6393f76510fc3415843f4428f137f13fa63b21f5
Kpotter9 <kjpu80129@gmail.com>
2024-07-17 01:11:25 -0400

    Update

--- meal-planner/src/App.js
+++ meal-planner/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/Home'
+import Nav from './components/nav/Nav'
+import {Route, Routes,Link} from "react-router-dom"
 
 function App() {
   
   return (
-    <div className="App">
-     
-     
-    </div>
-  );
+  <div className="App">
+  <Nav/>
+  <Routes>
+    
+    
+    <Route path='/' element={<Home/>}/>
+    <Route/>
+
+    
+
+  </Routes>
+  </div>)
 }
 
 export default App;
