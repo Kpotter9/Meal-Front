9bd75128abf5a5ba967229b2a4d6050f13875657
Kpotter9 <kjpu80129@gmail.com>
2024-08-06 23:14:40 -0400

    Update axiosConfig.js

--- meal-planner/src/api/axiosConfig.js
+++ meal-planner/src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://c226-71-62-148-33.ngrok-free.app",
-   headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: "https://friendly-alexine-potterworks-9098d577.koyeb.app/",
 
 });
